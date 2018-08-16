4f0226184735b598ec4daa5c167fbd1cb931b976
Kévin Clément <kevin_clement@outlook.fr>
2018-08-16 12:32:11 +0200

    Ajout filtre sessions archivées

--- imports/components/ui/IndexSessions.jsx
+++ imports/components/ui/IndexSessions.jsx
@@ -3,11 +3,15 @@ import PropTypes from 'prop-types'
 import { Session } from 'meteor/session';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
-import { BrowserRouter as Router, Route, Link, Switch }from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 
 import DetailsSession from './DetailsSession'
 
 export default class IndexSessions extends Component {
+    state = {
+        toggleSession: false,
+    }
+
     static propTypes = {
         sessions: PropTypes.array.isRequired,
         connecte: PropTypes.bool.isRequired,
@@ -20,9 +24,35 @@ export default class IndexSessions extends Component {
         sessions: [{}],
     };
 
+    renderSesssions() {
+        let sessionsFiltrees = this.props.sessions;
+        console.log(sessionsFiltrees)
+        if (this.state.toggleSession) {
+            sessionsFiltrees = sessionsFiltrees.filter((session) => { return (session.etat == "archivee") })
+        }
+        else {
+            sessionsFiltrees = sessionsFiltrees.filter((session) => { return !(session.etat == "archivee") })
+        }
+        console.log(sessionsFiltrees)
+        return sessionsFiltrees.map((session) => (
+            <div key={session._id}>
+                <Link to={`/sessions/${session._id}`}>
+                    {session.titre}
+                </Link>
+                <button onClick={() => Meteor.call('sessions.remove', session._id)}>Supprimer la session</button>
+                <br />
+            </div>
+        ))
+    }
+
+    handleChange(event) {
+        event.preventDefault();
+        const prevState = this.state.toggleSession;
+        this.setState({ toggleSession: !prevState })
+    }
     render() {
         //Conflit avec les props passées par la route sans déconstruction. Trouver une solution plus propre
-        var {match, path, ...rest} = this.props
+        var { match, path, ...rest } = this.props
         return (
             <div className="index-sessions">
                 <div className="index-sessions-gauche">
@@ -31,20 +61,23 @@ export default class IndexSessions extends Component {
                     </div>
                     <hr />
                     <div className="liste-sessions">
-                        {this.props.sessions.map((session) => (
-                            <div key={session._id}>
-                                <Link to={`/sessions/${session._id}`}>
-                                    {session.titre}
-                                </Link>
-                                <button onClick={() => Meteor.call('sessions.remove', session._id)}>Supprimer la session</button>
-                                <br />
-                            </div>
-                        ))}
+                        <label className="hide-archivee">
+                            <input type="checkbox" checked={this.state.toggleSession} readOnly onClick={this.handleChange.bind(this)} />
+                            Afficher les sessions archivées
+                        </label>
+                        {/* <label className="hide-archivee">
+                            <input
+                                type="checkbox"
+                                readOnly
+                                checked={this.state.hideCompleted}
+                                onClick={this.toggleHideCompleted.bind(this)}
+                            /> */}
+                        {this.renderSesssions()}
                     </div>
                 </div>
                 <hr />
                 <div className="index-sessions-droite">
-                    <Route path="/sessions/:sessionId" render={(props) => <DetailsSession {...props} {...rest} /> } />
+                    <Route path="/sessions/:sessionId" render={(props) => <DetailsSession {...props} {...rest} />} />
                 </div>
             </div>
         )
