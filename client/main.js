2b5ab4a91acdd0c93a74db1f6407a6567d511546
jeremien <jeremie.nuel@gmail.com>
2018-10-15 09:03:11 +0200

    pull

--- client/main.js
+++ client/main.js
@@ -9,17 +9,18 @@ import i18n from 'meteor/universe:i18n'
 
 import './main.html';
 import App from '../imports/components/App';
+// import TestAPI from '../imports/components/ui/TestAPITestAPI'
 /**
  * Debut du programme
  */
 Meteor.startup(() => {
   // Socket io client
   const PORT = 8080;
-  let socket = require('socket.io-client')(`http://localhost:${PORT}`);
+  let socket = require('socket.io-client')(`http://192.168.5.129:${PORT}`);
 
   socket.on('connect', function () {
     // console.log('Client connected');
-    render( < App socket={socket} /> , document.getElementById('root'))
+    render( <App socket={socket} /> , document.getElementById('root'))
   });
   socket.on('disconnect', function () {
     // console.log('Client disconnected');
