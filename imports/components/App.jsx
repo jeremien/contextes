01ef2b13a0d54098da4b98cfb78d89ce877fd397
Kevin Clement <kevin@asus-kevin.home>
2019-01-22 18:28:06 +0100

    Ajout module Login Web / local

--- imports/components/App.jsx
+++ imports/components/App.jsx
@@ -37,6 +37,8 @@ import LandingPage from './data/LandingPage';
 import { Layout, notification } from 'antd';
 
 import "antd/dist/antd.css";
+// import ConnexionSession from './VersionWeb/ConnexionSession';
+import LoginWeb from './VersionWeb/LoginWeb';
 
 const { Header, Content } = Layout;
 
@@ -66,26 +68,26 @@ class Application extends Component {
   componentDidMount() {
     window.addEventListener("beforeunload", this.handleLeavePage);
     Streamy.on('logoutForce', this.logoutForce.bind(this));
-    Streamy.on('onAir', () => this.setState({ onAir : true }));
+    Streamy.on('onAir', () => this.setState({ onAir: true }));
     Streamy.on('offAir', () => this.setState({ onAir: false }));
 
     // notifications
 
     Streamy.on('notification', (infos) => {
-      const {title, message, type} = infos;
+      const { title, message, type } = infos;
       this.openNotification(title, message, type);
     });
   }
 
   openNotification(title, message, type) {
 
-      notification.config({
-        placement : "bottomLeft"
-      }),
+    notification.config({
+      placement: "bottomLeft"
+    }),
 
       notification[type]({
-        message : title,
-        description : message
+        message: title,
+        description: message
       })
 
   }
@@ -95,7 +97,6 @@ class Application extends Component {
   }
 
   logoutForce() {
-    console.log('logout')
     localStorage.clear();
     Session.clear()
     Meteor.call('connexions.remove', this.props.connexion._id);
@@ -107,21 +108,25 @@ class Application extends Component {
 
 
   render() {
-    if (this.props.connecte) {
+    var propsToPass;
+    if (this.props.connecte && !this.props.loading) {
       Meteor.call('connexions.socket', this.props.connexion._id, Streamy.id())
       // const ConnexionContext = React.createContext(this.props.connexion);
     }
-    const { role, utilisateur, session, ...rest } = this.props.connexion
-    const propsToPass = { 
+    const { role, username, session, ...rest } = this.props.connexion
+
+    propsToPass = {
       connecte: this.props.connecte,
-      userSession: session, 
-      userId: this.props.connexion._id, 
-      role: role || "", 
-      utilisateur: utilisateur || "", 
+      userSession: session,
+      userId: this.props.connexion._id,
+      role: role || "",
+      utilisateur: username || "",
       socketId: Streamy.id(),
       loading: this.props.loading,
       onAir: this.state.onAir
     }
+
+
 
     return (
       <Router>
@@ -131,15 +136,14 @@ class Application extends Component {
           <Header style={{ backgroundColor: 'white', position: 'fixed', zIndex: 1, width: '100%' }}>
 
             <Route path="/" render={(props) => <TopBarContainer {...props} {...propsToPass} />} />
-
           </Header>
 
           <Content style={{ padding: '20px 50px', margin: '100px 0 0 0 ' }}>
 
+
             <Route exact path="/sessions" render={(props) => <IndexSessionsContainer {...props} {...propsToPass} />} />
-            <Route path="/sessions/:sessionId" render={(props) => <DetailsSession {...props} {...propsToPass} />} />
-            <Route exact path="/session/:idSession/chapitre/:idChapitre" render={(props) => <DetailsChapitreContainer {...props} {...propsToPass} draft={false}/>} />
-            <Route path="/session/:idSession/chapitre/:idChapitre/draft" render={(props) => <DetailsChapitreContainer {...props} {...propsToPass} draft={true}/>} />
+            <Route exact path="/session/:idSession/chapitre/:idChapitre" render={(props) => <DetailsChapitreContainer {...props} {...propsToPass} draft={false} />} />
+            <Route path="/session/:idSession/chapitre/:idChapitre/draft" render={(props) => <DetailsChapitreContainer {...props} {...propsToPass} draft={true} />} />
 
             <Route path="/publications" render={(props) => <IndexPublicationsContainer {...props} {...propsToPass} />} />
             <Route exact path="/publication/:idPublication" render={(props) => <DetailsPublicationsContainer {...props} {...propsToPass} layout={false} />} />
@@ -149,47 +153,90 @@ class Application extends Component {
             <Route path="/logs" render={(props) => <IndexLogsContainer {...props} {...propsToPass} />} />
 
             <Route exact path="/" render={(props) => <LandingPage {...props} {...propsToPass} />} />
-            <Route path="/login" render={(props) => <Login {...props} {...propsToPass} />} />
+
+
+            {/**
+            * Route pour la version Web
+            */}
+            {/* <Route path="/login" render={(props) => <Login {...props} {...propsToPass} />} /> */}
+            <Route path="/sessions/:sessionId" render={(props) => <DetailsSession {...props} {...propsToPass} />} />
+
+            {/**
+            * Route pour la versino serveur local
+            */}
+            <Route path="/login" render={(props) => <LoginWeb {...props} {...propsToPass} />} />
+            {/* <Route path="/sessions/:sessionId" render={(props) => <ConnexionSession {...props} {...propsToPass} />} /> */}
+
+
+
 
             <Route path="/test" render={(props) => <TestAPI {...this.props} {...propsToPass} />} />
 
           </Content>
         </Layout>
 
-      </Router>
+      </Router >
     )
   }
 };
+/**
+ * Tracker pour la version serveur local
+ */
 
+// export default withTracker((props) => {
+//   if (localStorage.getItem('userId') || Session.get('userId')) {
+//     const id = Session.get('userId') || localStorage.getItem('userId');
+//     if (!Session.get('userId')) { Session.set('userId', id) };
+//     const connexionsHandle = Meteor.subscribe('connexions');
+//     const loading = !connexionsHandle.ready()
+//     const connexion = Connexions.findOne(localStorage.getItem('userId'))
+//     const connexionExists = !loading && !!connexion
+//     if (!connexionExists) {
+//       if (!loading) {
+//         Session.clear();
+//         localStorage.clear();
+//       }
+//       return {
+//         loading: false,
+//         connecte: false,
+//         connexion: {},
+//       }
+//     }
+//     else {
+//       return {
+//         loading,
+//         connecte: connexionExists,
+//         connexion: connexionExists ? connexion : {},
+//       }
+//     }
+//   }
+
+
+//   else {
+//     return {
+//       loading: false,
+//       connecte: false,
+//       connexion: {},
+//     }
+//   }
+
+// })(Application);
+
+/**
+ * Tacker pour la version web
+ */
 export default withTracker((props) => {
-  if (localStorage.getItem('userId') || Session.get('userId')) {
-    const id = Session.get('userId') || localStorage.getItem('userId');
-    if (!Session.get('userId')) { Session.set('userId', id) };
-    const connexionsHandle = Meteor.subscribe('connexions');
-    const loading = !connexionsHandle.ready()
-    const connexion = Connexions.findOne(localStorage.getItem('userId'))
-    const connexionExists = !loading && !!connexion
-    if (!connexionExists) {
-      if (!loading) {
-        Session.clear();
-        localStorage.clear();
-      }
-      return {
-        loading: false,
-        connecte: false,
-        connexion: {},
-      }
-    }
-    else {
-      return {
-        loading,
-        connecte: connexionExists,
-        connexion: connexionExists ? connexion : {},
-      }
+  const connexionsHandle = Meteor.subscribe('connexions');
+  const loading = !connexionsHandle.ready()
+  if (!!Meteor.user()) {
+    const user = Meteor.user()
+    const connexion = Connexions.findOne(Meteor.userId())
+    return {
+      loading: loading,
+      connecte: !!Meteor.user(),
+      connexion: connexion ? connexion : {}
     }
   }
-
-
   else {
     return {
       loading: false,
@@ -197,6 +244,4 @@ export default withTracker((props) => {
       connexion: {},
     }
   }
-
 })(Application);
-
