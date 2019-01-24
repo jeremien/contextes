b6bcc11f623331cfc51edec30dc17375385fc94f
Kevin Clement <kevin_clement@outlook.fr>
2019-01-24 17:56:31 +0100

    Component de connexion aux sessions

--- imports/components/ui/ConnexionSession.jsx
+++ imports/components/ui/ConnexionSession.jsx
@@ -1,29 +1,94 @@
 import React from 'react'
 import { Meteor } from 'meteor/meteor';
+import { Roles } from 'meteor/alanning:roles';
+import DetailsChapitreContainer from '../data/DetailsChapitreContainer';
+import DetailsSession from './DetailsSession';
+import { Form, Input, Button, Select, Icon, message, Switch } from 'antd';
+const FormItem = Form.Item;
+const Option = Select.Option;
 
-export default class ConnexionSession extends React.Coomponent {
-    state = {
-        essai = false,
-        password: '',
-    };
+import { withTracker } from 'meteor/react-meteor-data';
+
+class ConnexionSession extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            essai: false,
+            password: '',
+            isInSession: false
+        };
+        this.handlePassword = this.handlePassword.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    // componentDidMount() {
+    //     this.setState({isInSession: Roles.userIsInRole('admis', this.props.match.params.sessionId)})
+    // }
 
     handleSubmit() {
-        this.setState({ essai: false })
-        Meteor.call('connexions.session', this.props.match.params.sessionId, this.props.userId, this.state.password, function (result) {
+        // this.setState({ essai: false })
+        console.log('submit')
+        Meteor.call('sessions.connexion', Meteor.userId(), this.props.match.params.sessionId, this.state.password, function (error, result) {
+           alert('error', error)
+            alert('result', result)
             if (!result) {
-                this.setState({ essai: true, password: '', })
+                // this.setState({ essai: true, password: '', })
             }
         })
     }
 
+    handlePassword(e) {
+        e.preventDefault()
+        this.setState({ password: e.target.value })
+    }
+
     render() {
-        return (
-            <div>
-                <form>
-                    <input type='password' value={this.state.password} />
-                    <button type="submit" value="envoyer" placeholder="envoyer" />
-                </form>
-            </div>
-        )
+        if (this.props.autorise) {
+            return (
+                <DetailsSession {...this.props} />
+            )
+        }
+        else {
+            return (
+                // <div>
+                //     <form>
+                //         <input type='password' value={this.state.password} onChange={this.handlePassword}/>
+                //         <button onSubmit={this.handleSubmit} type="submit" value="envoyer" placeholder="envoyer" />
+                //     </form>
+                // </div>
+                <Form
+                    layout='inline'
+                    onSubmit={this.handleSubmit}
+                >
+
+                    <FormItem>
+
+                        <Input
+                            placeholder='password'
+                            prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                            value={this.state.password}
+                            onChange={this.handlePassword}
+                            name='password'
+                        />
+
+                    </FormItem>
+                    <FormItem>
+                        <Button
+                            type='primary'
+                            htmlType='submit'
+                        >
+                            Envoyer
+                    </Button>
+                    </FormItem>
+                </Form>
+            )
+        }
+
     }
 }
+
+export default withTracker((props) => {
+    const autorise = !!(Roles.userIsInRole(Meteor.user(), 'admis', props.match.params.sessionId))
+    // const autorise = false
+    return { autorise: autorise }
+})(ConnexionSession)
