67be5548aa242e10261c51af8538c4550ca0cb81
jeremien <jeremie.nuel@gmail.com>
2019-03-19 19:02:56 +0100

    suppression de la branche images

--- imports/components/utils/Images.js
+++ imports/components/utils/Images.js
@@ -4,7 +4,6 @@ const getImageLink = (document) => {
 
     if (document.image !== null) {
         let img = Images.findOne({_id: document.image });
-        console.log(img.link('small'))
         return img ? img.link('small') : null;
     } else {
         return null;
